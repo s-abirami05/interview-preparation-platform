594330ccfb33d061c528a8cb6acc32f95d14ea07
M-kanmani <kanmanimurugesan1526@gmail.com>
2026-07-11 10:46:57 +0530

    Meaningful commit message

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Interview Preparation Platform Backend Running"
+  });
+});
+
+export default app;
